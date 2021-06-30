9ce7145edc3aff3f0792972b78d9bd9d18513f52
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:04:04 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center">Animals</h1>
+                <div className="row">
+                    <div className="col-3">
+                        <div className="border border-info p-2">
+                            <h3>Lion</h3>
+                            <h4>Panthero Lio</h4>
+                            <h4>140kg</h4>
+                            <h5>meat</h5>
+                            <button className="btn btn-warning">More info</button>
+
+                        </div>
+                    </div>
+                    <div className="col-3 offset-1 ">
+                        <div className="border border-info p-2">
+                            <h3>Gotilla</h3>
+                            <h4>Gorilla Beringei</h4>
+                            <h4>170kg</h4>
+                            <h5>plants</h5>
+                            <button className="btn btn-warning">More info</button>
+
+                        </div>
+                    </div>
+                    <div className="col-3 offset-1">
+                        <div className="border border-info p-2">
+                            <h3>Zebra</h3>
+                            <h4>Equeas quanga</h4>
+                            <h4>160kg</h4>
+                            <h5>plants</h5>
+                            <button className="btn btn-warning">More info</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
